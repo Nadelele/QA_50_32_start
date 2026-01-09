48b1fc3b5ae6617b64e2fd79534449f3f9acb80f
Nadelele <nadele@gmail.com>
2026-01-09 11:58:45 +0200

    start cssSelectors

---
+++ src/test/java/SelectorCSS.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SelectorCSS {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void phoneBookTest() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.get("https://telranedu.web.app/home");
+        driver.navigate().to("https://telranedu.web.app/home");
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        btnAbout.click();
+       // WebElement divRoot = driver.findElement(By.cssSelector("#root"));
+        //WebElement divRoot = driver.findElement(By.id("root"));
+        //WebElement divRoot = driver.findElement(By.cssSelector("div[id='root]"));
+        //WebElement divRoot = driver.findElement(By.cssSelector("*[id='root]"));
+        WebElement divRootClass = driver.findElement(By.cssSelector("div[class='container']"));
+       // WebElement divRootClass = driver.findElement(By.cssSelector("*[class='container']"));
+        // WebElement divRootClass = driver.findElement(By.cssSelector(".container"));
+        System.out.println(divRootClass.getAttribute("id"));
+        //System.out.println(divRoot.getAttribute("class"));
+        pause(5);
+
+        //driver.navigate().refresh();
+        driver.navigate().to("https://telranedu.web.app/login");
+        pause(5);
+        driver.navigate().back();
+        //driver.close(); // closes one tab
+        driver.quit(); //closes all tabs and browser
+    }
+
+    static void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
